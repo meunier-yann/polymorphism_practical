dbc8efaf1c82299083a3ad61b895d8956dced0d2
Yann Meunier <yann.meunier@insa-lyon.fr>
2023-12-04 17:48:27 +0100

    Ajout de la classe Yoshi

---
+++ src/Yoshi.h
@@ -0,0 +1,16 @@
+#ifndef YOSHI_H_
+#define YOSHI_H_
+
+
+#include "Character.h"
+#include <string>
+
+class Yoshi : public Character{
+  public:
+    Yoshi() = default;
+    ~Yoshi() = default;
+    std::string WhatAmI() const override;
+};
+
+
+#endif // YOSHI_H_
