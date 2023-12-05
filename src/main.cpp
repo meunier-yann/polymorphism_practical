e38b6f56aebdd3f4d2f8c2323f04accdc33d619e
Yann Meunier <yann.meunier@insa-lyon.fr>
2023-12-05 14:42:19 +0100

    Implémentation de la méthode test_race avec itérateurs

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "Mario.h"
 #include "Yoshi.h"
 #include <string>
+#include <vector>
 
 
 void test_create_character();
@@ -10,6 +11,7 @@ void test_accelerate();
 void test_break();
 void test_Mario();
 void test_Yoshi();
+void test_race();
 
 
 int main (){
@@ -17,12 +19,13 @@ int main (){
   // test_accelerate();
   // test_break();
   //test_Mario();
-  test_Yoshi();
+  //test_Yoshi();
+  test_race();
   return EXIT_SUCCESS;
 }
 
 // void test_create_character(){
-//   Character s;
+//   Character s; //Création ne marche plus mtn que la classe est abstracte
 //   std::cout << " max speed de: " << s.max_speed() << " et speed: " << s.speed()<< std::endl;
 // }
 //
@@ -35,7 +38,7 @@ int main (){
 // }
 //
 // void test_break(){
-//   Character s;
+//   Character s; //Création ne marche plus mtn que la classe est abstracte
 //   s.Accelerate();
 //   s.Accelerate();
 //   s.Accelerate();
@@ -55,5 +58,31 @@ void test_Yoshi(){
   std::cout << y.WhatAmI() << y.max_speed() << std::endl;
   y.Accelerate();y.Accelerate();y.Accelerate();y.Accelerate();y.Accelerate();y.Accelerate();y.Accelerate();
   std::cout << y.speed()<< std::endl;
+}
+
+void test_race(){
+  std::vector<Character*> racers;
+  racers.push_back(new Yoshi);
+  racers.push_back(new Mario);
+
+  float vitesse = 0;
+  std::string gagnant = "";
+
+  
+  std::cout << "Méthode avec itérateurs" << std::endl;
+  for (int i = 1; i< 4; ++i){
+    for (auto it = begin (racers); it != end (racers); ++it) {
+      (*it)->Accelerate ();
+      std::cout << "Après " << i << " tour(s), la vitesse de " << (*it)->WhatAmI() << " est de "
+                                                               << (*it)->speed() << std::endl;
+      if ((*it)->speed() > vitesse) {
+        gagnant = (*it)->WhatAmI();
+        vitesse = (*it)->speed();
+      }
+
+    }
+  }
+
+  std::cout << "Le gagnant est " << gagnant <<  '\n';
 
 }
